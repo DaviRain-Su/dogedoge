49d0d91bff955899ec861fa5b32be3f38bf16528
Davirain-Su <davirain.yin@gmail.com>
2021-02-01 10:09:50 +0800

    example warp todos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+use std::env;
+use warp::Filter;
+
+mod filters;
+mod handlers;
+mod models;
+
+#[cfg(test)]
+mod tests;
+
+/// Provides a RESTful web server managing some Todos.
+///
+/// API will be:
+///
+/// - `GET /todos`: return a JSON list of Todos.
+/// - `POST /todos`: create a new Todo.
+/// - `PUT /todos/:id`: update a specific Todo.
+/// - `DELETE /todos/:id`: delete a specific Todo.
+#[tokio::main]
+async fn main() {
+    if env::var_os("RUST_LOG").is_none() {
+        // Set `RUST_LOG=todos=debug` to see debug logs,
+        // this only shows access logs.
+        env::set_var("RUST_LOG", "todos=info");
+    }
+    pretty_env_logger::init();
+
+    let db = models::blank_db();
+
+    let api = filters::todos(db);
+
+    // View access logs by setting `RUST_LOG=todos`.
+    let routes = api.with(warp::log("todos"));
+    // Start up the server...
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
